98dc39667e52f0ace9526042bca212101f78a8ae
ajja94 <anja@getacademy.no>
2019-11-05 09:32:54 +0100

    05.11.2019

--- SteinSaksPapir/ScriptTilSSP.js
+++ SteinSaksPapir/ScriptTilSSP.js
@@ -1,25 +1,19 @@
 // JavaScript source code
-let userScore = 0;
-let computerScore = 0;
+//hjelpevariabler
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
-const scoreBoard_div = document.querySelector(".score-board");
-const result_p = document.querySelector(".result >p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
+//model
+let userScore = 0;
+let computerScore = 0;
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+let yourMove = null;
+let computerMove = null;
 
-function getComputerChoice() {
-    const choices = ['r', 'p', 's'];
-    const randomNumber = Math.floor(Math.random() * 3);
-    return choices[randomNumber];
-}
-
-function convertToWord(letter) {
-    if (letter === "r") return "Rock";
-    if (letter === "p") return "Paper";
-    return "Scissors";
-}
+//view
 
 function win(userChoice, computerChoice) {
     userScore++;
@@ -40,6 +34,22 @@ function draw(userChoice, computerChoice) {
     computerScore_span.innerHTML = computerScore;
     result_p.innerHTML = `${convertToWord(userChoice)} equals ${convertToWord(computerChoice)}. Its a DRAW`
 }
+
+
+
+//controller
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function convertToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
 
 function game(userChoice) {
     const computerChoice = getComputerChoice();
